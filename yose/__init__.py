220a3195e56aea92bcbc95c810ccb0d95378bbb6
Eric Mignot <eric.mignot@gmail.com>
2018-08-24 10:26:44 -0700

    Power of two

--- yose/__init__.py
+++ yose/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, make_response, jsonify, request
 from challenges.ping import pong
 
 app = Flask(__name__)
@@ -11,3 +11,18 @@ def alive():
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/primeFactors')
+def primeFactors():
+    number = int(request.args.get('number'))
+    decomposition = []
+    while number % 2 == 0 :
+        decomposition.append(2)
+        number = number / 2
+
+    return make_response(jsonify(
+        {
+            'number': int(request.args.get('number')),
+            'decomposition': decomposition
+        }
+    ))
